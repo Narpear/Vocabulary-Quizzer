8e65349b0ff6d5824b3c60f004c307e15f934911
Prerana-Kulkarni <92934847+Prerana-Kulkarni@users.noreply.github.com>
2022-01-13 23:32:46 +0530

    Update ScrapedWords.py

--- ScrapedWords.py
+++ ScrapedWords.py
@@ -1,4 +1,5 @@
 # json is JavaScript Object Notation
+from textwrap import wrap
 import requests, random
 
 import json
@@ -8,142 +9,19 @@ r = requests.get(url)
 
 questions = []
 answers = []
-options1 = []
-options2 = []
-options3 = []
-options4 = []
-options5 = []
+options=[]
 
 
-words_and_meanings =  random.choices(r.json()['data'], k=5)
+for i in range (0,10):
+    words_and_meanings =  random.choices(r.json()['data'], k=4)
+    x = random.randint(1,4)
+    questions.append(words_and_meanings[x-1]["name"])
+    answers.append(x)
+    temp_options = [words_and_meanings[0]["detail"], words_and_meanings[1]["detail"], words_and_meanings[2]["detail"], words_and_meanings[3]["detail"]]
+    options.append(temp_options)
 
-set1 = words_and_meanings[0]
-set2 = words_and_meanings[1]
-set3 = words_and_meanings[2]
-set4 = words_and_meanings[3]
-set5 = words_and_meanings[4]
+lists = ['question', 'answer', 'options']
 
-word1 = set1["name"]
-
-meaning1 = set1["detail"]
-meaning2 = set2["detail"]
-meaning3 = set3["detail"]
-meaning4 = set4["detail"]
-meaning5 = set5["detail"]
-
-
-questions.append(word1)
-answers.append(meaning1)
-options1.append(meaning1)
-options1.append(meaning2)
-options1.append(meaning3)
-options1.append(meaning4)
-options1.append(meaning5)
-
-words_and_meanings =  random.choices(r.json()['data'], k=5)
-
-set1 = words_and_meanings[0]
-set2 = words_and_meanings[1]
-set3 = words_and_meanings[2]
-set4 = words_and_meanings[3]
-set5 = words_and_meanings[4]
-
-word2 = set1["name"]
-
-meaning1 = set1["detail"]
-meaning2 = set2["detail"]
-meaning3 = set3["detail"]
-meaning4 = set4["detail"]
-meaning5 = set5["detail"]
-
-
-questions.append(word2)
-answers.append(meaning1)
-options2.append(meaning1)
-options2.append(meaning2)
-options2.append(meaning3)
-options2.append(meaning4)
-options2.append(meaning5)
-
-words_and_meanings =  random.choices(r.json()['data'], k=5)
-
-set1 = words_and_meanings[0]
-set2 = words_and_meanings[1]
-set3 = words_and_meanings[2]
-set4 = words_and_meanings[3]
-set5 = words_and_meanings[4]
-
-word3 = set1["name"]
-
-meaning1 = set1["detail"]
-meaning2 = set2["detail"]
-meaning3 = set3["detail"]
-meaning4 = set4["detail"]
-meaning5 = set5["detail"]
-
-
-questions.append(word3)
-answers.append(meaning1)
-options3.append(meaning1)
-options3.append(meaning2)
-options3.append(meaning3)
-options3.append(meaning4)
-options3.append(meaning5)
-
-words_and_meanings =  random.choices(r.json()['data'], k=5)
-
-set1 = words_and_meanings[0]
-set2 = words_and_meanings[1]
-set3 = words_and_meanings[2]
-set4 = words_and_meanings[3]
-set5 = words_and_meanings[4]
-
-word4 = set1["name"]
-
-meaning1 = set1["detail"]
-meaning2 = set2["detail"]
-meaning3 = set3["detail"]
-meaning4 = set4["detail"]
-meaning5 = set5["detail"]
-
-
-questions.append(word4)
-answers.append(meaning1)
-options4.append(meaning1)
-options4.append(meaning2)
-options4.append(meaning3)
-options4.append(meaning4)
-options4.append(meaning5)
-
-words_and_meanings =  random.choices(r.json()['data'], k=5)
-
-set1 = words_and_meanings[0]
-set2 = words_and_meanings[1]
-set3 = words_and_meanings[2]
-set4 = words_and_meanings[3]
-set5 = words_and_meanings[4]
-
-word5 = set1["name"]
-
-meaning1 = set1["detail"]
-meaning2 = set2["detail"]
-meaning3 = set3["detail"]
-meaning4 = set4["detail"]
-meaning5 = set5["detail"]
-
-
-questions.append(word5)
-answers.append(meaning1)
-options5.append(meaning1)
-options5.append(meaning2)
-options5.append(meaning3)
-options5.append(meaning4)
-options5.append(meaning5)
-
-
-#print(questions)
-#print(answers)
-
-options = [options1, options2, options3, options4, options5]
-
-#print(options)
+data = {"question": questions, "answer": answers, "options": options }
+with open('data1.json', 'w') as database:
+    json.dump(data, database, indent=4)
